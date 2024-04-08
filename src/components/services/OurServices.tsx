0281c463aa13b1d4bf95be2c92c059f8b407479a
warlock <fecypher@gmail.com>
2024-04-08 15:35:01 +0100

    changed write up

--- src/components/services/OurServices.tsx
+++ src/components/services/OurServices.tsx
@@ -14,7 +14,7 @@ const services = [
     id: 1,
     title: "Personal Loan",
     details:
-      "International Neo Bank makes installment loans at all of our full-service bank locations, with friendly, helpful loan officers.",
+      "International leo Bank makes installment loans at all of our full-service bank locations, with friendly, helpful loan officers.",
     icon: <HelpingHandIcon size={40} color="#fff" />,
   },
   {
